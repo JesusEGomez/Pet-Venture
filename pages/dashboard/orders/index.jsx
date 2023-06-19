80e4fb45b8b9bdecf300d823459e09ce566bcae0
TomasHas <thastrup@gmail.com>
2023-06-19 19:09:42 -0300

    dashboard finde

--- pages/dashboard/orders/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function OrdersDash() {
-  return <div>OrdersDash</div>;
-}
-
-export default OrdersDash;
---
+++ src/app/Components/Dashboard/Orders.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./orders.module.css";
+
+function Orders() {
+  return (
+    <div className={styles.cartel}>
+      <h1 className={styles.orders}>ORDERS</h1>
+    </div>
+  );
+}
+
+export default Orders;
